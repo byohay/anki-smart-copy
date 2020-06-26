1e0a6515bee5541fae6046f25f779561c7399e80
Ben <benyohay91@gmail.com>
2020-06-26 14:50:38 +0300

    SmartCopyDefinition: Name fields appropriately and document them.

--- __init__.py
+++ __init__.py
@@ -9,19 +9,47 @@ from aqt.utils import showInfo
 
 source_field_name = "Word (in Kanji/Hanzi)"
 
-# TODO: 1. Identify note somehow.
-#       2. Allow multiple notes to copy from.
-
-
 @dataclass(frozen=True)
 class SmartCopyDefinition:
-  source_field_name: str
-  destination_field_name: str
+  # Name of the field in the reference note to copy from.
+  field_name_to_copy_from: str
+
+  # Name of the field in the new note to copy to.
+  field_name_to_copy_to: str
+
+  # Name of the model of the reference note to copy from.
   model_name: str
+
+  # `True` if the contents of the field `source_field_name` that appears in the contents of the
+  # field `field_name_to_copy_from` should be blanked out after copying, `False` otherwise.
+  #
+  # Example:
+  # - The contents of `source_file_name` = "foo"
+  # - The contents of `field_name_to_copy_from` = "foobar"
+  # The result would be: `___bar`.
   blank_out_word_after_copy: bool
-  add_only_if_not_empty: bool
+
+  # `True` if the copy should occur only when the field is not empty, `False` in case the copy
+  # should anyway occur.
+  copy_only_if_field_not_empty: bool
+
+  # All substrings in the contents of the field `field_name_to_copy_from` that match `regex_remove`
+  # will be removed when copied to `field_name_to_copy_to`.
   regex_remove: Optional[str] = None
-  blank_out_word_indicator: Optional[str] = None
+
+  # The contents of the field `field_name_to_copy_from` will be copied such that the first match to
+  # the regex `blank_out_word_regex` will be blanked out. Has no effect if
+  # `blank_out_word_after_copy` is `False` or this is `None`.
+  #
+  # Useful in case the word in `source_file_name` doesn't appear in its original form, but in some
+  # conjugation.
+  #
+  # Example:
+  # - The contents of `source_file_name` = "foo"
+  # - The contents of `field_name_to_copy_from` = "<b>fooie</b>bar"
+  # - `blank_out_word_regex` is equal to `r".*<b>(.*?)</b>.*"`.
+  # The result would be: `_____bar`.
+  blank_out_word_regex: Optional[str] = None
 
 smart_copy_definitions = [
   SmartCopyDefinition(
@@ -90,8 +118,8 @@ def smart_copy(changed, note, current_field_index):
   note_changed = False
 
   for definition in smart_copy_definitions:
-    source = definition.source_field_name
-    destination = definition.destination_field_name
+    source = definition.field_name_to_copy_from
+    destination = definition.field_name_to_copy_to
 
     note_to_copy_from = (
       _get_note_from_reference_card_with_model(reference_cards, definition.model_name)
@@ -111,7 +139,7 @@ def smart_copy(changed, note, current_field_index):
     source_value = _sentence_after_blanking_out_word(source_value, definition, text_to_search)
 
     if source_value not in note[destination]:
-      if note[destination] and definition.add_only_if_not_empty:
+      if note[destination] and definition.copy_only_if_field_not_empty:
         continue
 
       if not note[destination]:
@@ -141,10 +169,10 @@ def _sentence_after_blanking_out_word(source_value, smart_copy_definition, text_
   if not smart_copy_definition.blank_out_word_after_copy:
     return source_value
 
-  if not smart_copy_definition.blank_out_word_indicator:
+  if not smart_copy_definition.blank_out_word_regex:
     return re.sub(text_to_search, "_" * len(text_to_search), source_value)
 
-  blank_out_match = re.match(smart_copy_definition.blank_out_word_indicator, source_value)
+  blank_out_match = re.match(smart_copy_definition.blank_out_word_regex, source_value)
 
   if not blank_out_match:
     return re.sub(text_to_search, "_" * len(text_to_search), source_value)
@@ -159,7 +187,7 @@ def _model_is_correct_type(model):
     fields = mw.col.models.fieldNames(model)
 
     destination_fields_names = (
-      [definition.destination_field_name for definition in smart_copy_definitions]
+      [definition.field_name_to_copy_to for definition in smart_copy_definitions]
     )
 
     return (source_field_name in fields and
